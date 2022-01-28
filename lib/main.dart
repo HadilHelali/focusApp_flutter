91061a42c3daaf80e2c64b253bc7924488df1fab
HadilHelali <helalihadil4@gmail.com>
2022-01-28 22:42:43 +0100

    Added the Screen2 as well as its assets then tested out in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,44 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:example/Screen2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('focus app'),
-        centerTitle: true,
-        backgroundColor:Colors.indigo[200],
-      ),
-      body:Container(
-        decoration: BoxDecoration(
-          color: Colors.black,
-        ),
-        child: Center(
-          child: Text(
-              'hello',
-            ),
-        ),
-        ),
-
-    )
-
-  ));
-
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Focus App',
+      home: Screen2(),
+    );
+  }
+}
+
+/* void main() {
+  runApp(
+      MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('focus app'),
+      centerTitle: true,
+      backgroundColor: Colors.indigo[200],
+    ),
+    body: Container(
+      decoration: const BoxDecoration(
+        color: Colors.black,
+      ),
+      child: const Center(
+        child: Text(
+          'hello',
+        ),
+      ),
+    ),
+  ))
+      );
+} */
